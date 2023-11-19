cdad492ad8c9e6b297e3c99537d0e9005a664feb
itai <102740785+NoamMesh@users.noreply.github.com>
2023-11-19 12:00:46 +0200

    h

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { Link, NavLink, Route, Routes, useLocation, BrowserRouter } from "react-router-dom"
+import * as React from 'react';
+import Box from '@mui/material/Box';
 
-import "./styles/index.css"; // relevant for the NavLinks in this code
-import { makeStyles } from "@mui/material";
-
-const useStyles = makeStyles((theme) => ({}))
 export default function App() {
-    
   return (
-    <>
-      
-    </>
-  )
+    <Box
+      sx={{
+        minHeight: '100vh', // Minimum height of the viewport
+        width: '100vw', // Width of the viewport
+        backgroundImage: `url(${process.env.PUBLIC_URL + '/images/bg.jpg'})`,
+        backgroundSize: 'cover', // Cover the entire area
+        backgroundPosition: 'center', // Center the image
+        backgroundRepeat: 'no-repeat', // Do not repeat the image
+      }}
+    >
+      {/* Your content here */}
+    </Box>
+  );
 }
-
