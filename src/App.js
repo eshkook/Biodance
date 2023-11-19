136df5fac71557b18dc89fb4ae3e2390d6285afe
itai <102740785+NoamMesh@users.noreply.github.com>
2023-11-19 12:43:10 +0200

    h

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Header from './components/Header';
+import PlaceToVisit from './components/PlaceToVisit';
 
 export default function App() {
   return (
-    <Box
-      sx={{
-        minHeight: '100vh', // Minimum height of the viewport
-        width: '100vw', // Width of the viewport
-        backgroundImage: `url(${process.env.PUBLIC_URL + '/images/bg.jpg'})`,
-        backgroundSize: 'cover', // Cover the entire area
-        backgroundPosition: 'center', // Center the image
-        backgroundRepeat: 'no-repeat', // Do not repeat the image
-      }}
-    >
-      {/* Your content here */}
-    </Box>
+    <>
+      <CssBaseline />
+      <Box
+        sx={{
+          minHeight: '100vh', // Minimum height of the viewport
+          width: '100vw', // Width of the viewport
+          backgroundImage: `url(${process.env.PUBLIC_URL + '/images/bg.jpg'})`,
+          backgroundSize: 'cover', // Cover the entire area
+          backgroundPosition: 'center', // Center the image
+          backgroundRepeat: 'no-repeat', // Do not repeat the image
+        }}
+      >
+        {/* Your content here */}
+      </Box>
+    </>
   );
 }
