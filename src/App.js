5a9f9b7cfd019954737410f64a8daf6d38781d4b
itai <102740785+NoamMesh@users.noreply.github.com>
2023-11-19 11:17:08 +0200

    n

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Link, NavLink, Route, Routes, useLocation, BrowserRouter } from "react-router-dom"
 
 import "./styles/index.css"; // relevant for the NavLinks in this code
+import { makeStyles } from "@mui/material";
 
+const useStyles = makeStyles((theme) => ({}))
 export default function App() {
     
-
   return (
     <>
       
