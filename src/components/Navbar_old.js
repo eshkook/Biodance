b7ed942e1fadc22dd793f878019e8dcc741f95b7
itai <102740785+NoamMesh@users.noreply.github.com>
2023-12-16 18:51:56 +0200

    א

--- src/components/Navbar_old.js
+++ src/components/Navbar_old.js
@@ -5,15 +5,17 @@ import Toolbar from '@mui/material/Toolbar';
 import SortIcon from '@mui/icons-material/Sort';
 import { Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Navbar({ homeLink = "/" }) {
+
     const navigate = useNavigate();
 
     const handleHomeClick = () => {
         if (window.location.pathname === homeLink) {
-            navigate(0); // This will refresh the current page
+            window.location.reload();
         } else {
-            navigate(homeLink); // Navigate to different route
+            navigate(homeLink);
         }
     };
 
@@ -38,3 +40,27 @@ export default function Navbar({ homeLink = "/" }) {
         </>
     );
 }
+//     return (
+//         <>
+//             <AppBar position="fixed" sx={{ background: 'none', boxShadow: 'none' }}>
+//                 {/* The standard height of a Material-UI AppBar is 64 pixels for desktop screens. */}
+//                 <Toolbar sx={{
+//                     display: 'flex',
+//                     alignItems: 'center',
+//                     justifyContent: 'space-between',
+//                     width: '80%',
+//                     margin: '0 auto'
+//                 }}>
+//                     <Link to={homeLink} style={{ textDecoration: 'none' }}>
+//                         <Typography variant="h5" sx={{ color: '#fff', fontFamily: "'Nunito', sans-serif" }}>
+//                             Biodanz<span style={{ color: '#14c484' }}>App</span>
+//                         </Typography>
+//                     </Link>
+//                     <IconButton>
+//                         <SortIcon sx={{ color: '#fff', fontSize: '2rem' }} />
+//                     </IconButton>
+//                 </Toolbar>
+//             </AppBar>
+//         </>
+//     );
+// }
