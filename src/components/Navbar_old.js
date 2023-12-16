a7cd06ce71e0f89398c59fc54953f718594d8ac1
itai <102740785+NoamMesh@users.noreply.github.com>
2023-12-16 19:06:52 +0200

    d

--- src/components/Navbar_old.js
+++ src/components/Navbar_old.js
@@ -4,24 +4,14 @@ import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
 import SortIcon from '@mui/icons-material/Sort';
 import { Typography } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar({ homeLink = "/" }) {
-
-    const navigate = useNavigate();
-
-    const handleHomeClick = () => {
-        if (window.location.pathname === homeLink) {
-            window.location.reload();
-        } else {
-            navigate(homeLink);
-        }
-    };
 
     return (
         <>
             <AppBar position="fixed" sx={{ background: 'none', boxShadow: 'none' }}>
+                {/* The standard height of a Material-UI AppBar is 64 pixels for desktop screens. */}
                 <Toolbar sx={{
                     display: 'flex',
                     alignItems: 'center',
@@ -29,9 +19,11 @@ export default function Navbar({ homeLink = "/" }) {
                     width: '80%',
                     margin: '0 auto'
                 }}>
-                    <Typography variant="h5" sx={{ color: '#fff', fontFamily: "'Nunito', sans-serif", cursor: 'pointer' }} onClick={handleHomeClick}>
-                        Biodanz<span style={{ color: '#14c484' }}>App</span>
-                    </Typography>
+                    <NavLink to={(window.location.pathname === homeLink) ? 0 : homeLink} style={{ textDecoration: 'none' }}>
+                        <Typography variant="h5" sx={{ color: '#fff', fontFamily: "'Nunito', sans-serif" }}>
+                            Biodanz<span style={{ color: '#14c484' }}>App</span>
+                        </Typography>
+                    </NavLink>
                     <IconButton>
                         <SortIcon sx={{ color: '#fff', fontSize: '2rem' }} />
                     </IconButton>
@@ -40,27 +32,3 @@ export default function Navbar({ homeLink = "/" }) {
         </>
     );
 }
-//     return (
-//         <>
-//             <AppBar position="fixed" sx={{ background: 'none', boxShadow: 'none' }}>
-//                 {/* The standard height of a Material-UI AppBar is 64 pixels for desktop screens. */}
-//                 <Toolbar sx={{
-//                     display: 'flex',
-//                     alignItems: 'center',
-//                     justifyContent: 'space-between',
-//                     width: '80%',
-//                     margin: '0 auto'
-//                 }}>
-//                     <Link to={homeLink} style={{ textDecoration: 'none' }}>
-//                         <Typography variant="h5" sx={{ color: '#fff', fontFamily: "'Nunito', sans-serif" }}>
-//                             Biodanz<span style={{ color: '#14c484' }}>App</span>
-//                         </Typography>
-//                     </Link>
-//                     <IconButton>
-//                         <SortIcon sx={{ color: '#fff', fontSize: '2rem' }} />
-//                     </IconButton>
-//                 </Toolbar>
-//             </AppBar>
-//         </>
-//     );
-// }
