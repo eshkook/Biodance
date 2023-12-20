33960ea43d179956d5692b60cf002e82f1505357
itai <102740785+NoamMesh@users.noreply.github.com>
2023-12-20 16:22:14 +0200

    d

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,38 +6,30 @@ import { Link, NavLink } from "react-router-dom"
 
 export default function Hero() {
     return (
-
-        // <Box sx={{
-        //     maxWidth: '400px',
-        //     margin: '0 auto',
-        //     display: 'flex',
-        //     flexDirection: 'column',
-        //     alignItems: 'center', // Center aligns all children horizontally 
-        //     justifyContent: 'center', // Center aligns all children vertically (if needed)
-        // }}>
-
-        <Box
-            sx={{
-                minHeight: '100vh',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center', // Changed to 'stretch' to make the children (cards) of the same height
-                flexDirection: { xs: 'column', md: 'row' }, // Responsive design: column layout for small screens, row layout for medium and larger screens
-                // p: 300, // Padding for the box
-            }}
-            id="hero"
-        >
-            <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                {/* uncomment: */}
-                {/* <NavLink to="/home" state={{ no_failure_message: true }}> */}
-                {/* delete: */}
-                <NavLink to="/login" state={{ no_failure_message: true }}>
-                    <Button color="primary">Login</Button>
-                </NavLink>
-                <Link to={'/signup'} >
-                    <Button color="secondary">Sign Up</Button>
-                </Link>
-            </ButtonGroup>
-        </Box>
+        <>
+            <Box
+                sx={{
+                    minHeight: '100vh',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center', // Changed to 'stretch' to make the children (cards) of the same height
+                    flexDirection: { xs: 'column', md: 'row' }, // Responsive design: column layout for small screens, row layout for medium and larger screens
+                    // p: 300, // Padding for the box
+                }}
+                id="hero"
+            >
+                <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                    {/* uncomment: */}
+                    {/* <NavLink to="/home" state={{ no_failure_message: true }}> */}
+                    {/* delete: */}
+                    <NavLink to="/login" state={{ no_failure_message: true }}>
+                        <Button color="primary">Login</Button>
+                    </NavLink>
+                    <Link to={'/signup'} >
+                        <Button color="secondary">Sign Up</Button>
+                    </Link>
+                </ButtonGroup>
+            </Box>
+        </>
     );
 }
