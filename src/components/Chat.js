0df0a8aca54e5d869fdaf534683eb444d275cd08
itai <102740785+NoamMesh@users.noreply.github.com>
2024-02-15 14:17:06 +0200

    f

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import { useMutation } from "@tanstack/react-query";
-import { Box, Grid, TextField, Button, Typography, Button, CircularProgress } from '@mui/material';
+import { Box, Grid, TextField, Button, Typography, CircularProgress } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import Message from './Message';
 import { chat_post } from "./posts";
 
-export default function Chat({ chosen_language='Hebrew' }) {
+export default function Chat({ chosen_language = 'Hebrew' }) {
+
+    const [messages, setMessages] = useState([
+        { id: 1, text: "Hi there!", sender: "bot" },
+        { id: 2, text: "Get to work maggot", sender: "user" }
+    ]);
 
     const [errorMessage, setErrorMessage] = useState(null);
 
-    const confirmationMutation = useMutation({
+    const chatMutation = useMutation({
         mutationFn: chat_post,
         onSuccess: data => {
-            console.log('Response data:', data);
-            navigate("/login", { state: { message: 'Confirmation was successful, now you can log in!' } })
+            setMessages(messages => [...messages, { id: messages.length + 1, text: data, sender: "bot" }]);
         },
         onError: error => {
             setErrorMessage(error.message || "An error occurred");
@@ -22,13 +26,7 @@ export default function Chat({ chosen_language='Hebrew' }) {
     });
 
     const [formState, setFormState] = useState({
-        email: (location.state?.email) ? location.state.email : '',
-        confirmation_code: ''
-    })
-
-    const [fieldErrorState, setFieldError] = useState({
-        email: false,
-        confirmation_code: false,
+        message: ''
     })
 
     function updateFormState(event) {
@@ -41,99 +39,93 @@ export default function Chat({ chosen_language='Hebrew' }) {
 
     function handleSubmit(event) {
         event.preventDefault() // preventing re-rendering the page
-        const temp_object = {
-            email: !isValidEmail(formState.email),
-            confirmation_code: formState.confirmation_code == ''
-        }
-        setFieldError(temp_object)
-
-        if (!(temp_object.email || temp_object.confirmation_code)) {
-
-            console.log(formState)
-
-            confirmationMutation.mutate({
-                email: formState.email,
-                confirmation_code: formState.confirmation_code,
+        if (formState.message) {
+            setMessages(messages => [...messages, { id: messages.length + 1, text: formState.message, sender: "user" }]);
+            chatMutation.mutate({
+                message: formState.message,
             });
-        } else {
-            setErrorMessage("Fields in red are invalid")
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-    const [messages, setMessages] = useState([
-        { id: 1, text: "Hi there!", sender: "bot" },
-        { id: 2, text: "Get to work maggot", sender: "user" }
-    ]);
-    const [input, setInput] = useState('');
-
-    const handleSend = () => {
-        if (input.trim() !== "") {
-            // Add the new message to the messages array
-            setMessages([...messages, { id: messages.length + 1, text: input, sender: "user" }]);
-            setInput(""); // Clear the input field
-        }
-    };
-
-    const handleInputChange = (event) => {
-        setInput(event.target.value);
-    };
 
     return (
         <>
             <Box
                 sx={{
+                    maxWidth: '600px', // or any appropriate max width
+                    margin: '0 auto', // centers the box
+                    paddingBottom: '20px', // gives some space at the bottom
                     // minHeight: '100vh',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center', // Changed to 'stretch' to make the children (cards) of the same height
-                    flexDirection: { xs: 'column', md: 'row' }, // Responsive design: column layout for small screens, row layout for medium and larger screens
+                    // display: 'flex',
+                    // justifyContent: 'center',
+                    // alignItems: 'center', // Changed to 'stretch' to make the children (cards) of the same height
+                    // flexDirection: { xs: 'column', md: 'row' }, // Responsive design: column layout for small screens, row layout for medium and larger screens
                     // p: 300, // Padding for the box
                 }}
             >
+                {errorMessage && (
+                    <>
+                        <Typography variant="body2" color="error">
+                            {errorMessage}
+                        </Typography>
+                        <br />
+                    </>
+                )}
                 <Box sx={{ height: "100vh", display: "flex", flexDirection: "column", bgcolor: "grey.200" }}>
                     <Box sx={{ flexGrow: 1, overflow: "auto", p: 2 }}>
                         {messages.map((message) => (
                             <Message key={message.id} message={message} chosen_language={chosen_language} />
                         ))}
                     </Box>
-                    <Box sx={{ p: 2, backgroundColor: "background.default" }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={10}>
-                                <TextField
-                                    fullWidth
-                                    size="small"
-                                    placeholder="Type a message"
-                                    variant="outlined"
-                                    value={input}
-                                    onChange={handleInputChange}
-                                />
-                            </Grid>
-                            <Grid item xs={2}>
-                                <Button
-                                    fullWidth
-                                    size="large"
-                                    color="primary"
-                                    variant="contained"
-                                    endIcon={<SendIcon />}
-                                    onClick={handleSend}
-                                >
-                                    Send
-                                </Button>
-                            </Grid>
-                        </Grid>
-                    </Box>
+                    <form onSubmit={handleSubmit} noValidate autoComplete='off'>
+                        {/* noValidate makes the browser not use its built-in validation messages as we want to do it ourselves,
+          autoComplete off makes it not complete the user's text */}
+
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                gap: '10px'
+                            }}
+                        >
+                            <TextField
+                                onChange={updateFormState}
+                                id="message-input"
+                                label="Type your message here..."
+                                variant="outlined"
+                                name="message"
+                                value={formState.message}
+                                fullWidth // makes the TextField full width
+                                sx={{ mt: 1 }} // adds margin-top for spacing
+                                autoFocus={true}
+                            />
+                            <Button
+                                variant="contained"
+                                color="primary" // use a theme color that indicates primary action
+                                type='submit'
+                                disabled={chatMutation.isLoading}
+                                endIcon={!chatMutation.isLoading && <SendIcon />}
+                                sx={{ mt: 1 }} // adds margin-top for spacing
+                            >
+                                {chatMutation.isLoading ? <CircularProgress size={24} /> : "Send"}
+                            </Button>
+
+                            {/* <TextField
+                                onChange={updateFormState}
+                                id="message-input"
+                                label="Your message"
+                                variant="outlined"
+                                name="message"
+                                value={formState.message}
+                                autoFocus={true}
+                            />
+                            <Button
+                                variant="contained"
+                                type='submit'
+                                disabled={chatMutation.isLoading}>
+                                {chatMutation.isLoading ? <CircularProgress size={24} /> : "Submit"}
+                            </Button> */}
+                        </Box>
+                    </form>
                 </Box>
             </Box>
         </>
